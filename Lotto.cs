7b8d2b5f03c39d993b23cb4564cb89a1f634c9d4
Reni98 <93825160+Reni98@users.noreply.github.com>
2024-02-01 12:42:50 +0100

    Add files via upload

---
+++ Lotto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas_desktop
+{
+    internal class Lotto
+    {
+        public int sorszam;
+        public int szam1;
+        public int szam2;
+        public int szam3;
+        public int szam4;
+        public int szam5;
+
+        public Lotto(string sorszam, string szam1, string szam2, string szam3, string szam4, string szam5)
+        {
+            this.sorszam = int.Parse(sorszam);
+            this.szam1 = int.Parse(szam1);
+            this.szam2 = int.Parse(szam2);
+            this.szam3 = int.Parse(szam3);
+            this.szam4 = int.Parse(szam4);
+            this.szam5 = int.Parse(szam5);
+        }
+    }
+}
---
+++ Sorsolas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas_desktop
+{
+    internal class Sorsolas
+    {
+        public int szam;
+        public int db;
+
+        public Sorsolas(int szam, int db)
+        {
+            this.szam = szam;
+            this.db = db;
+        }
+    }
+}
